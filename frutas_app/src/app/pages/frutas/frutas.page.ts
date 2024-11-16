5958404ba828fe294c8efe67e63a48cd37b3a588
AlexisObregon31 <alexisobregon31@gmail.com>
2024-11-16 01:37:12 -0400

    CRUD Frutas

---
+++ frutas_app/src/app/pages/frutas/frutas.page.ts
@@ -0,0 +1,125 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FrutasService } from '../../services/frutas.service';
+
+interface Fruta {
+  id_fruta: number;
+  nombre: string;
+}
+
+@Component({
+  selector: 'app-frutas',
+  templateUrl: './frutas.page.html',
+  styleUrls: ['./frutas.page.scss'],
+})
+export class FrutasPage implements OnInit {
+  frutas: Fruta[] = [];
+  nueva_fruta: Fruta = { id_fruta: 0, nombre: '' };
+  esEdicion: boolean = false; // Bandera para saber si se está editando
+
+  constructor(private frutasService: FrutasService) {}
+
+  ngOnInit() {
+    this.obtenerFrutas();
+  }
+
+  obtenerFrutas() {
+    this.frutasService.getFrutas().subscribe((frutas) => {
+      this.frutas = frutas;
+    });
+  }
+
+  cargarFrutaEnFormulario(fruta: Fruta) {
+    this.nueva_fruta = { ...fruta }; // Cargar la fruta en el formulario
+    this.esEdicion = true; // Activar modo edición
+  }
+
+  guardarFruta() {
+    if (!this.nueva_fruta.nombre) return;
+
+    if (this.esEdicion) {
+      // Modo edición
+      this.frutasService.updateFruta(this.nueva_fruta.id_fruta, this.nueva_fruta).subscribe(() => {
+        this.frutas = this.frutas.map((f) =>
+          f.id_fruta === this.nueva_fruta.id_fruta ? { ...this.nueva_fruta } : f
+        );
+        this.limpiarFormulario();
+      });
+    } else {
+      // Modo agregar
+      this.frutasService.createFruta(this.nueva_fruta).subscribe((fruta) => {
+        this.frutas.push(fruta);
+        this.limpiarFormulario();
+      });
+    }
+  }
+
+  eliminarFruta(id_fruta: number) {
+    this.frutasService.deleteFruta(id_fruta).subscribe(() => {
+      this.frutas = this.frutas.filter((fruta) => fruta.id_fruta !== id_fruta);
+    });
+  }
+
+  limpiarFormulario() {
+    this.nueva_fruta = { id_fruta: 0, nombre: '' };
+    this.esEdicion = false;
+  }
+}
+
+
+
+/* Viejo
+import { Component, OnInit } from '@angular/core';
+import { FrutasService } from '../../services/frutas.service';
+
+interface Fruta {
+  id_fruta: number;
+  nombre: string;
+}
+
+@Component({
+  selector: 'app-frutas',
+  templateUrl: './frutas.page.html',
+  styleUrls: ['./frutas.page.scss'],
+})
+export class FrutasPage implements OnInit {
+  frutas: Fruta[] = [];
+
+  nueva_fruta: Fruta = { id_fruta: 0, nombre: ''};
+
+  constructor(private frutasService: FrutasService) { }
+
+  ngOnInit() {
+    this.obtenerFrutas();
+  }
+
+  obtenerFrutas() {
+    this.frutasService.getFrutas().subscribe(frutas => {
+      this.frutas = frutas;
+    });
+  }
+
+  agregarFruta() {
+    if (!this.nueva_fruta.nombre ) return;
+
+    this.frutasService.createFruta(this.nueva_fruta).subscribe(fruta => {
+      this.frutas.push(fruta);
+      this.nueva_fruta = { id_fruta: 0, nombre: ''}; // Limpiar el formulario
+    });
+  }
+
+  
+  eliminarFruta(id_fruta: number) {
+    this.frutasService.deleteFruta(id_fruta).subscribe(() => {
+      this.frutas = this.frutas.filter(fruta => fruta.id_fruta !== id_fruta);
+    });
+  }
+  
+
+  actualizarFruta(id_fruta: number) {
+    const fruta = this.frutas.find(f => f.id_fruta === id_fruta);
+    if (fruta) {
+      this.frutasService.updateFruta(id_fruta, fruta).subscribe();
+    }
+  }
+}*/
---
+++ frutas_app/src/app/services/frutas.service.ts
@@ -0,0 +1,96 @@
+//import { Axios } from './../../../node_modules/axios/index.d';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, from, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Axios, AxiosResponse } from 'axios';
+
+
+interface Fruta {
+  id_fruta: number;
+  nombre: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FrutasService {
+  private apiUrl = 'http://192.168.0.141:3828/api/';
+
+  constructor(private http: HttpClient) { } //private axios: Axios
+
+  // Obtener todas las frutas
+  getFrutas(): Observable<Fruta[]> {
+    return this.http.get<Fruta[]>(`${this.apiUrl}get_frutas`);
+  }
+
+  // Obtener una fruta por id_fruta
+  getFruta(id_fruta: number): Observable<Fruta> {
+    return this.http.get<Fruta>(`${this.apiUrl}/${id_fruta}`);
+  }
+
+  // Crear una nueva fruta
+  createFruta(fruta: Fruta): Observable<Fruta> {
+    return this.http.post<Fruta>(`${this.apiUrl}create_fruta`, fruta);
+  }
+
+  // Actualizar una fruta existente
+  updateFruta(id_fruta: number, fruta: Fruta): Observable<Fruta> {
+    return this.http.put<Fruta>(`${this.apiUrl}update_fruta/${id_fruta}`, fruta);
+  }
+
+  // Eliminar una fruta
+  deleteFruta(id_fruta: number): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}delete_fruta/${id_fruta}`,null);
+  }
+
+  /*
+    getFrutas(): Observable<Fruta[]> {
+      return from(this.axios.get(this.apiUrl)).pipe(
+        map(response => response.data as Fruta[]),
+        catchError((error) => {
+          console.error('Error al obtener frutas:', error);
+          return of([]); // Devuelve un arreglo vacío en caso de error
+        })
+      );
+    }
+
+    getFrutaByid_fruta(id_fruta: number): Observable<Fruta[]> {
+      return from(this.axios.get(`${this.apiUrl}/${id_fruta}`)).pipe(
+        map(response => response.data as Fruta[]),
+        catchError((error) => {
+          console.error('Error al obtener fruta por id_fruta:', error);
+          return of([]); // Devuelve un arreglo vacío en caso de error
+        })
+      );
+    }
+
+
+    // Crear una nueva fruta
+    createFruta(fruta: Fruta): Observable<Fruta[]> {
+      return from(this.axios.post(`${this.apiUrl}`, fruta)).pipe(
+        map(response => response.data as Fruta[]),
+        catchError((error) => {
+          console.error('Error al crear fruta por id_fruta:', error);
+          return of([]); // Devuelve un arreglo vacío en caso de error
+        })
+      );
+    }
+
+    // Actualizar una fruta existente
+    updateFruta(id_fruta: number, fruta: Fruta): Observable<Fruta> {
+      return from(this.axios.put(`${this.apiUrl}/${id_fruta}`, fruta)).pipe(
+        map(response => response.data as Fruta[]),
+        catchError((error) => {
+          console.error('Error al actualizar fruta por id_fruta:', error);
+          return of([]); // Devuelve un arreglo vacío en caso de error
+        })
+      );
+    }
+
+    // Eliminar una fruta
+    deleteFruta(id_fruta: number): Observable<void_fruta> {
+      return this.http.delete<void_fruta>(`${this.apiUrl}/${id_fruta}`);
+    }
+  */
+}
